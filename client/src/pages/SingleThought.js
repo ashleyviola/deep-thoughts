a4d1d0ea4258d165671fd8caea0d3b1d437493f1
Ashley <ashleyviola@Ashleys-MacBook-Pro.local>
2022-02-28 19:36:35 -0500

    updated final files

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+
+import ReactionList from '../components/ReactionList';
+import ReactionForm from '../components/ReactionForm';
+
+import Auth from '../utils/auth';
 import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHT } from '../utils/queries';
-import ReactionList from '../components/ReactionList';
 
-const SingleThought = props => {
-
+const SingleThought = (props) => {
   const { id: thoughtId } = useParams();
-  
+
   const { loading, data } = useQuery(QUERY_THOUGHT, {
-    variables: { id: thoughtId }
+    variables: { id: thoughtId },
   });
 
   const thought = data?.thought || {};
@@ -24,14 +27,19 @@ const SingleThought = props => {
         <p className="card-header">
           <span style={{ fontWeight: 700 }} className="text-light">
             {thought.username}
-          </span>{'  '}
+          </span>{' '}
           thought on {thought.createdAt}
         </p>
         <div className="card-body">
           <p>{thought.thoughtText}</p>
         </div>
       </div>
-      {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
+
+      {thought.reactionCount > 0 && (
+        <ReactionList reactions={thought.reactions} />
+      )}
+
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
     </div>
   );
 };
--- server/server.js
+++ server/server.js
@@ -1,48 +1,36 @@
 const express = require('express');
+const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
 
-// import ApolloServer
-const { ApolloServer } = require('apollo-server-express');
-
-// import our typeDefs and resolvers 
-const {typeDefs, resolvers } = require('./schemas');
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
-
-const { authMiddleware } = require('./utils/auth');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 const startServer = async () => {
-  // create a new Apollo server and pass in our schema data
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: authMiddleware
+    context: authMiddleware,
   });
-  
-  // start the Apollo server
   await server.start();
-
-  //integrate our Apollo server with the express application as middleware
   server.applyMiddleware({ app });
-
-  // log where we can go to test our GQL API
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 };
 
-// initialize the apollo server
-startServer();
+startServer()
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Serve up static assets 
+// Serve up static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req,res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
